bea172c4ef5e3393e8be8aa342f352ac52d0b2a8
toky <tokyfydev@gmail.com>
2024-03-01 21:41:01 +0300

    finish

--- _test_.c
+++ _test_.c
@@ -2,16 +2,16 @@
 #include <stdio.h>
 
 
-#define _FMT_ "|%c|"
-
+#define _FMT_ "|%+50.3s|"
+#define _ARG_ "1.2"
 
 int main()
 {
   int ft_p = 0;
   char p = 'a';
 
-  ft_p =  ft_printf(_FMT_ , p);
+  ft_p =  ft_printf(_FMT_ , _ARG_);
           ft_printf("%d \n" , ft_p);
-  p =     printf(_FMT_,p);
+  p =     printf(_FMT_,_ARG_);
           printf("%d \n" ,p);
 }
--- ft_put_c.c
+++ ft_put_c.c
@@ -5,6 +5,5 @@ int ft_put_c(t_modifier *modifier , char chr)
   char s[2] = " ";
   *s = chr;
   modifier->precision = 1;
-  ft_put_s(modifier, s);
-  return 1;
+  return ft_put_s(modifier, s);
 }
